3e84f6197e89f64b1bed955eabffa1e6a5491f7c
omerentro <omer.bar@entro.security>
2024-07-02 13:37:06 +0300

    change name

--- web3-hardhat/scripts/deploy.ts
+++ web3-hardhat/scripts/deploy.ts
@@ -1,16 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-
   const Contract = await ethers.getContractFactory("CampaignFactory");
   const contract = await Contract.deploy();
-
   const address = await contract.getAddress()
-
-  console.log("CrowdFundingFactory deployed to:", address);
+  console.log("CampaignFactory deployed to:", address);
 }
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
