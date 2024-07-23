e654db24f5fef4f74d341a2c193baa6ca1407d95
omerentro <omer.bar@entro.security>
2024-07-23 19:35:20 +0300

    updating for sepolina

--- web3-hardhat/hardhat.config.ts
+++ web3-hardhat/hardhat.config.ts
@@ -1,9 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
 };
+
+dotenv.config()
+const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   ...config,
@@ -15,6 +19,10 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
   },
 };
 
